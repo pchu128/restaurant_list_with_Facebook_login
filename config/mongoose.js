d608ea6123fdef5a4a529f1e8dc88a3edbb644e9
Peter C <peter03311@gmail.com>
2020-06-07 23:05:17 +0800

    URL -> URI

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-mongoose.connect(process.env.MONGODB_URL, { 
+mongoose.connect(process.env.MONGODB_URI, { 
   useNewUrlParser: true, 
   useUnifiedTopology: true,
   useCreateIndex: true

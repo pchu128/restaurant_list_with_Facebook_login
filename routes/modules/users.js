37d90ef61793af84fa5ddbd326fa58faac70209b
Peter C <peter03311@gmail.com>
2020-06-07 19:00:47 +0800

    feat: add user registration function

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,13 +1,29 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/users')
 
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post('/login', (req, res) => {
-  
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email }).then( user => {
+    if (user) {
+      console.log('此信箱已經註冊過囉！')
+      res.render('register', {
+        name, email, password, confirmPassword
+      })
+    } else {
+      return User.create({
+        name, email, password
+      })
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+    }
+  })
 })
+
 router.get('/register', (req, res) => {
   res.render('register')
 })

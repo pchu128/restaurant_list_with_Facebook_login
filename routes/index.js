e0fd2c0436239ecd41f54c79c6a99b2a94812350
Peter C <peter03311@gmail.com>
2020-06-07 18:49:19 +0800

    add user login page

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const restaurant = require('./modules/restaurant')
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/restaurants', restaurant)
+router.use('/users', users)
 
 module.exports = router

e939fb14bc993d42da58a8e2b3431e3ebae531a9
Peter C <peter03311@gmail.com>
2020-06-07 23:31:27 +0800

    upload heroku

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const usePassport = require('./config/passport')
 require('./config/mongoose')
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')

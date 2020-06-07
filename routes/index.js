3109b72485ead7bca8edc512d3926eee79ca4a29
Peter C <peter03311@gmail.com>
2020-06-07 19:42:43 +0800

    add authentication in middleware and index

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,10 @@ const router = express.Router()
 const home = require('./modules/home')
 const restaurant = require('./modules/restaurant')
 const users = require('./modules/users')
+const { authenticator } = require('../middleware/auth')
 
-router.use('/', home)
-router.use('/restaurants', restaurant)
 router.use('/users', users)
+router.use('/restaurants', restaurant)
+router.use('/', authenticator, home)
 
 module.exports = router
